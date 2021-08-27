7946acc068400342751315f66e527eb41f136edf
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-27 16:35:51 -0500

    Se crea la entidad cliente

---
+++ src/main/java/com/ead/prueba/Cliente.java
@@ -0,0 +1,32 @@
+package com.ead.prueba;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+
+
+@Data
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "id_cliente")
+	private int id;
+	@Column(name = "nombre")
+	private String nombre;
+	@Column(name = "apellido")
+	private String apellido;
+	@Column(name = "cedula")
+	private String cedula;
+	@Column(name = "celular")
+	private String celular;
+}
