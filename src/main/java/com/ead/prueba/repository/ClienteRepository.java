4e080bab96b5705a70598b89f73c63b9a1289621
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-30 16:36:22 -0500

    Se modifico la interfaz cliente repositorio

--- src/main/java/com/ead/prueba/repository/ClienteRepository.java
+++ src/main/java/com/ead/prueba/repository/ClienteRepository.java
@@ -6,13 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import com.ead.prueba.entities.Cliente;
-
 
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
 
-	@Transactional(readOnly = true)
-	Optional<Cliente> findByUsername(String username);
 }
