a918dfdbe58521ff450ceab205c7f5339701c4bf
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-30 11:30:37 -0500

    Se creo la clase MHelpers

---
+++ src/main/java/com/ead/prueba/utils/helpers/MHelpers.java
@@ -0,0 +1,11 @@
+package com.ead.prueba.utils.helpers;
+
+import org.modelmapper.ModelMapper;
+
+public class MHelpers {
+
+	public static ModelMapper modelMapper() {
+		
+		return new ModelMapper();
+	}
+}
