b997d8f50416f72fb60a317dbdba014a40fbb023
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-30 16:40:43 -0500

    Se creo el paquete exceptions de nuevo
    se creo el paquete de nuevo, pero en la carpeta utils

---
+++ src/main/java/com/ead/prueba/utils/exceptions/ApiUnprocessableEntity.java
@@ -0,0 +1,18 @@
+package com.ead.prueba.utils.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class ApiUnprocessableEntity extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ApiUnprocessableEntity(String message) {
+		
+		super(message);
+	}
+}
