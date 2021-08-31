a001019c254a38abc6887b4a5e5ed8748cd73344
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-31 11:15:01 -0500

    Se modifico la clase Auth Service

--- src/main/java/com/ead/prueba/services/implementation/AuthService.java
+++ src/main/java/com/ead/prueba/services/implementation/AuthService.java
@@ -1,23 +1,45 @@
 package com.ead.prueba.services.implementation;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ead.prueba.dto.JwtResponse;
-
-
+import com.ead.prueba.dto.JwtUserDTO;
+import com.ead.prueba.security.JwtIO;
+import com.ead.prueba.utils.helpers.DateUtils;
 
 @Service
 public class AuthService {
 
-public JwtResponse login(String clientId, String clientSecret) {
+	@Autowired
+	private JwtIO jwtIO;
+	@Autowired
+	private DateUtils dateUtils;
 
+	@Value("${ead.jwt.token.expires-in}")
+	private int EXPIRES_IN;
 
+	public JwtResponse login(String clientId, String clientSecret) {
+
+		UUID uid = UUID.randomUUID();
+		
+		JwtUserDTO user = JwtUserDTO.builder()
+				.name("Estefania")
+				.lastname("Aguirre")
+				.role("ADMIN")
+				.country("Colombia")
+				.uid(uid.toString())
+				.build();
+		
 		JwtResponse jwt = JwtResponse.builder()
 				.tokenType("bearer")
-				.accessToken("kajushdiffhf")
-				.issuedAt(System.currentTimeMillis()+"")
+				.accessToken(jwtIO.generateToken(user))
+				.issuedAt(dateUtils.getDateMillis() +"")
 				.cliendId(clientId)
-				.expiresIn(3600)
+				.expiresIn(EXPIRES_IN)
 				.build();
 
 		return jwt;
