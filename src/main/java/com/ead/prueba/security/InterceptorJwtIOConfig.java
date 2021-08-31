37b064a6af6d40fcd9cfdfc99be1965bb18ee286
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-31 10:49:48 -0500

    Se creo la clase de configuracion intercepto Jwt

---
+++ src/main/java/com/ead/prueba/security/InterceptorJwtIOConfig.java
@@ -0,0 +1,26 @@
+package com.ead.prueba.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class InterceptorJwtIOConfig implements WebMvcConfigurer {
+
+	@Value("${ead.jwt.security.enabled:false}")
+	private boolean securityEnabled;
+
+	@Autowired
+	private InterceptorJwtIO interceptorJwtIO;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		if (securityEnabled) {
+			registry.addInterceptor(interceptorJwtIO);
+		}
+	}
+
+}
