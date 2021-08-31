e284cdb4eb95d868236ff9da3bed7c1d990b2e7d
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-31 12:04:11 -0500

    Se creo la interfaz Producto Service

---
+++ src/main/java/com/ead/prueba/services/interfaces/IProductoService.java
@@ -0,0 +1,27 @@
+package com.ead.prueba.services.interfaces;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ead.prueba.dto.ProductoDTO;
+import com.ead.prueba.dto.ProductoRequest;
+
+@Service
+public interface IProductoService {
+
+	List<ProductoDTO> findAll();
+
+	ProductoDTO findByProductType(String tipoProducto);
+
+	ProductoDTO findByProductId(int productoId);
+
+	void save(ProductoRequest producto);
+
+	void update(ProductoRequest producto, int productoId);
+
+	void saveAll(List<ProductoRequest> producto);
+
+	void deleteById(int productoId);
+
+}
