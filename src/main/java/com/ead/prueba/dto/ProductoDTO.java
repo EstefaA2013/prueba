4842c850e132ecef254b1c8d1918c53be25af2c8
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-31 11:48:11 -0500

    Se crea la clase ProductoDTO

---
+++ src/main/java/com/ead/prueba/dto/ProductoDTO.java
@@ -0,0 +1,26 @@
+package com.ead.prueba.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+import lombok.Data;
+
+@Data
+public class ProductoDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String tipoProducto;
+	private int cantidadProducto;
+	private Date fechaRegistro;
+	private Date fechaEntrega;
+	private double precioEnvio;
+	private String numeroGuia;
+	private int idCliente;
+	private int codigoTransporte;
+	
+}
