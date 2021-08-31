fe9550eeaf3f8a8f653dfa61fcfe18f5b869af6e
Estefania Aguirre <estefania.aguirre@ingeneo.com.co>
2021-08-31 11:55:19 -0500

    Se creo la clase Producto Request

---
+++ src/main/java/com/ead/prueba/dto/ProductoRequest.java
@@ -0,0 +1,28 @@
+package com.ead.prueba.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ProductoRequest implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@JsonProperty(value = "tipo_producto")
+	private String tipoProducto;
+	@JsonProperty(value = "cantidad_producto")
+	private int cantidadProducto;
+	@JsonProperty(value = "fecha_entrega")
+	private Date fechaEntrega;
+	@JsonProperty(value = "precio_envio")
+	private double precioEnvio;
+	@JsonProperty(value = "numero_guia")
+	private String numeroGuia;
+	@JsonProperty(value = "id_cliente")
+	private int idCliente;
+	@JsonProperty(value = "codigo_transporte")
+	private int codigoTransporte;
+}
